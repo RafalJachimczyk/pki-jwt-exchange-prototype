cb8661a2b445cc80e4fbeec99ee31e26cf1da486
Rafal Jachimczyk <rafal.jachimczyk@bbc.co.uk>
2018-02-08 20:42:05 +0000

    errrm

--- src/repository/serviceRepository.js
+++ src/repository/serviceRepository.js
@@ -2,7 +2,7 @@ var serviceRepository = function () {
     const serviceRegistry = [
         {
           'uuid': 'cddb01f1-46f1-4066-9414-b7467c9fe305',
-          'cn':  'rafal@jachimczyk.com' 
+          'cn':  'john@example.com' 
         },
         {
           'uuid': 'a8e43210-ac72-4d29-9195-8df970a5fe6a',
